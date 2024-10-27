b9ae5620e5da6ee143084de8d369fc56cd66b1e6
Julia Kuzmina <veh.gerli@mail.ru>
2024-10-27 05:24:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function toReadable (number) {
-          const ZERO_TO_NINETEEN = [
+            const ZERO_TO_NINETEEN = [
         "zero", "one", "two", "three", "four", "five",
         "six", "seven", "eight", "nine", "ten",
         "eleven", "twelve", "thirteen", "fourteen", "fifteen",
@@ -18,8 +18,10 @@ module.exports = function toReadable (number) {
     if (number==0)  return ZERO_TO_NINETEEN[0];
         
     if (number >= 100) {
-         readableNumber = ZERO_TO_NINETEEN[Math.floor(number/100)] + ' ' + RANK[0]+ ' ';
+         readableNumber = ZERO_TO_NINETEEN[Math.floor(number/100)] + ' ' + RANK[0];
          number%=100;
+         if (number == 0) return readableNumber;
+     else readableNumber+=' ';
     }
        
     if (number>=20 && number<100) {
