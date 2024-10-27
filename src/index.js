33e89ef90997783d749efcd4d6549f85de54bf60
Julia Kuzmina <veh.gerli@mail.ru>
2024-10-27 23:03:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,58 @@
-module.exports = function toReadable (number) {
-            const ZERO_TO_NINETEEN = [
+ const ZERO_TO_NINETEEN = [
         "zero", "one", "two", "three", "four", "five",
         "six", "seven", "eight", "nine", "ten",
         "eleven", "twelve", "thirteen", "fourteen", "fifteen",
         "sixteen", "seventeen", "eighteen", "nineteen"
         ];
-    const TY = [
+const TY = [
             "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
             ]
-    const RANK = [
-        "hundred"
+const RANK = [
+        "", "thousend", "million"
         ];
+        
+    if (!number) return ZERO_TO_NINETEEN[0];
 
     let readableNumber = "";
-    let lengthNumber = number.toString().length;
     
-    if (number==0)  return ZERO_TO_NINETEEN[0];
+    //выделяем трады
+    let triads = [];
+    let temp = number;
+    while(temp) {
+        triads.push(temp%1000);
+        temp=Math.floor(temp/1000);
+    }
+    triads = triads.reverse();
+    // console.log("Кол-во триад: " + triads.length + "\nТриады: " + triads)
+    
+    //исследуем каждую триаду
+    for (let i = 0; i<triads.length; i++) {
         
-    if (number >= 100) {
-         readableNumber = ZERO_TO_NINETEEN[Math.floor(number/100)] + ' ' + RANK[0];
-         number%=100;
-         if (number == 0) return readableNumber;
-     else readableNumber+=' ';
-    }
+        let triad = triads[i];
+        
+        if (triad.toString().length === 3) {
+         readableNumber += ZERO_TO_NINETEEN[Math.floor(triad/100)] + ' hundred';
+         triad%=100;
+        readableNumber+=' ';
+        }
        
-    if (number>=20 && number<100) {
-     readableNumber+=TY[+number.toString()[0]-2];
-     number%=10;
-     if (number == 0) return readableNumber;
-     else readableNumber+=' ';
+        if (triad.toString().length === 2) {
+            if (triad<20) {
+                readableNumber+=ZERO_TO_NINETEEN[triad%100];
+                triad=0;
+            }
+            else {
+                readableNumber+=TY[triad.toString()[0] - 2];
+                triad%=10;
+                
+            }
+            readableNumber+=' ';
+        }
+
+        if (triad!=0) readableNumber+=ZERO_TO_NINETEEN[triad] + ' ';
+    
+        if (triads[i]) readableNumber+=RANK[triads.length - 1 - i];
+        readableNumber+=' ';
     }
-
- if (number!=0)
-    readableNumber+=ZERO_TO_NINETEEN[number];
-
-    return readableNumber;
-}
+        
+    return readableNumber.trim();
