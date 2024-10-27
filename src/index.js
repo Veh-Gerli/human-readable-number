299ca1d4d53c5ddd943b5348f1641b83b71f11e7
Julia Kuzmina <veh.gerli@mail.ru>
2024-10-27 05:04:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function toReadable (number) {
-  
+      const ZERO_TO_NINETEEN = [
+        "zero", "one", "two", "three", "four", "five",
+        "six", "seven", "eight", "nine", "ten",
+        "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+        "sixteen", "seventeen", "eighteen", "nineteen"
+        ];
+    const TY = [
+            "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
+            ]
+    const RANK = [
+        "hundred"
+        ];
+
+    let readableNumber = "";
+    let stringNumber = number.toString();
+    
+    if (number >= 100) {
+         readableNumber = ZERO_TO_NINETEEN[Math.floor(number/100)] + ' ' + RANK[0]+ ' ';
+         number%=100;
+    }
+       
+    if (number>=20 && number<100) {
+     readableNumber+=TY[+number.toString()[0]-2] + ' ';
+     number%=10;
+    }
+    
+    if (number != 0) 
+    readableNumber+=ZERO_TO_NINETEEN[number];
+
+    return readableNumber;
 }
